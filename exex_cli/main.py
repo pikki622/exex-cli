8882dd875b87ab03d78f323f792059142499b720
Viktor Persson <viktor.persson@arcsin.se>
2019-11-16 15:06:29 +0100

    Refactor main.

--- exex_cli/main.py
+++ exex_cli/main.py
@@ -1,4 +1,7 @@
-from .app import application
+from exex_cli.application import application
+from exex_cli.commands import ExtractCommand
 
 if __name__ == "__main__":
+    application.add(ExtractCommand())
+    application.set_default_command("extract", is_single_command=True)
     application.run()
