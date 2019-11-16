1fa878789dad003fe7dc53f0574df5b151767d88
Viktor Persson <viktor.persson@arcsin.se>
2019-11-16 19:49:50 +0100

    Refactor convert util.

--- exex_cli/convert.py
+++ exex_cli/convert.py
@@ -3,15 +3,26 @@ from exex_cli.util import array_dimensions
 import json
 
 
-def to_csv(values, delimiter=",", line_separator="\n"):
-    width, height = array_dimensions(values)
+def to_strings(values):
+    outer_dim, inner_dim = array_dimensions(values)
 
-    if width == 0 and height == 0:
-        return values
-    elif height == 0:
-        return line_separator.join(values)
+    if outer_dim == 0 and inner_dim == 0:
+        return str(values)
+    elif inner_dim == 0:
+        return list(map(str, values))
     else:
-        return line_separator.join(delimiter.join(r) for r in values)
+        return [list(map(str, row)) for row in values]
+
+
+def to_csv(values, delimiter=",", line_separator="\n"):
+    outer_dim, inner_dim = array_dimensions(values)
+
+    if outer_dim == 0 and inner_dim == 0:
+        return values
+    elif inner_dim == 0:
+        return line_separator.join(to_strings(values))
+    else:
+        return line_separator.join(delimiter.join(to_strings(row)) for row in values)
 
 
 def to_json(values):
